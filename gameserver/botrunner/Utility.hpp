48248b6eb214ba9ef6949c41934d6fce6105c7ad
Dave Fletcher <fletch@monkeysatkeyboards.com>
2012-04-17 23:53:03 -0700

    Utility doesn't need to be a class, changed it to namespace.

--- gameserver/botrunner/Utility.hpp
+++ gameserver/botrunner/Utility.hpp
@@ -8,11 +8,10 @@
 #include <string>
 #include <sstream>
 
-class Utility {
-  public:
-    static int str2int(std::string s);
-    static double str2double(std::string s);
-    static int readfile(const char *path, std::stringstream &buf);
+namespace Utility {
+  int str2int(std::string s);
+  double str2double(std::string s);
+  int readfile(const char *path, std::stringstream &buf);
 };
 
 #endif /* [BOTRUNNER_UTILS] */
